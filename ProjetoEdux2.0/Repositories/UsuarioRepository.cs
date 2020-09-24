dfccb8d053133bbe6389597a5b283526924a9209
Guilherme <parra.guioliveira@hotmail.com>
2020-09-24 17:41:45 -0300

    Primeiro commit

---
+++ ProjetoEdux2.0/Repositories/UsuarioRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoEdux2._0.Repositories
+{
+    public class UsuarioRepository
+    {
+    }
+}
